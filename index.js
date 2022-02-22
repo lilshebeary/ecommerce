90c713e97dfb827de56d7d9889961dec86590e13
Ciri Ryan <cirijaye@yahoo.com>
2022-02-21 16:40:33 -0800

    add form and scratch pad

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
  const app = express();
 
  app.get('/', (req, res) => {
-    res.send('hi there!');
+    res.send(`
+        <div>
+            <form method="POST" autocomplete="off">
+                <input name="email" type="email" placeholder="email"  />
+                <input name="password" type="password" placeholder="password"  />
+                <input name="passwordConfirmation" type="password" placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+ });
+
+ app.post('/', (req, res) => {
+    res.send('Account Created!')
  });
 
  app.listen(3000, () => {
