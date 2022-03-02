711595fa861c29accd3feda8bf992603da6e4aee
Ciri Ryan <cirijaye@yahoo.com>
2022-03-02 05:36:39 -0800

    user search getOne

--- repositories/users.js
+++ repositories/users.js
@@ -40,16 +40,19 @@ class UsersRepository {
     randomId() {
         return crypto.randomBytes(4).toString('hex');
     };
+
+    async getOne(id) {
+        const records = await this.getAll();
+        return records.find(record => record.id === id );
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.create({ email: 'test.test.com', password: 'password' });
-
-    const users = await repo.getAll();
+    const user = await repo.getOne('4f2e769');
     
-    console.log(users);
+    console.log(user);
 };
 
 test();
